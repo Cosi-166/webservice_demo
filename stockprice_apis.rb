7b921664a5fbaad93b98d645dff9785b7e4493cb
Pito Salas <pitosalas@gmail.com>
2017-11-14 11:26:37 -0500

    some changes

--- stockprice_apis.rb
+++ stockprice_apis.rb
@@ -8,7 +8,7 @@ require 'csv'
 # Referece for the Quandli API: https://www.quandl.com/docs/api#basic-data-request
 # Example illustrates getting data out of an api, as json, and working with it
 
-result = Typhoeus.get("https://www.quandl.com/api/v3/datasets/WIKI/AMZN/data.json?order=asc&exclude_column_names=true&start_date=2012-01-01&end_date=2017-01-01&column_index=4&collapse=annual&transformation=rdiff")
+result = Typhoeus.get("https://www.quandl.com/api/v3/datasets/WIKI/TWTR/data.json?order=asc&exclude_column_names=true&start_date=2012-01-01&end_date=2017-01-01&column_index=4&collapse=annual&transformation=rdiff")
 
 puts "\n**** RAW JSON"
 ap result.response_body
